2203dd4b8cbb8e4d5d5607a70a3e921b835080a9
SaidislomSaidazimovv <saidazimovsaidislom97@gmail.com>
2024-10-24 22:53:25 +0500

    first commit

---
+++ src/component/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Button({ title, extraStyle, name, onclick }) {
+  return (
+    <button
+      onClick={onclick}
+      name={name}
+      className={`bg-primary-5 text-white w-full font-kubmh font-bold text-sm leading-[17.36px] py-3 border-[.5px] border-gray-100 rounded ${extraStyle}`}
+    >
+      {title}
+    </button>
+  );
+}
+
+export default Button;
